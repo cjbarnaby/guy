968b44c48ade01b3cbb5df9f3a19b913fad92afd
Christopher Barnaby <cjbarnaby@gmail.com>
2016-07-26 14:26:34 +1000

    shit is remarkably working

--- js/main.js
+++ js/main.js
@@ -20,13 +20,16 @@ var multi_image = {
 };
 
 $(document).ready(function() {
+    $(".overlay").hide();
 
     // EVENT HANDLERS FOR INTERNAL NAV LINKS
     $(".internal").on("click", function() {
+        $(".overlay").hide();
         $(".filter-box").addClass("filter");
         $(".filter-box").removeClass("unfilter");
         $(".internal").removeClass("active");
         $(this).addClass("active");
+        showOverlay($(this).attr("id"));
     });
 
     // EVENT HANDLERS FOR EXTERNAL NAV LINKS
@@ -38,9 +41,18 @@ $(document).ready(function() {
 
     // EVENT HANDLERS FOR PORTFOLIO LINKS
 
+    // EVEN HANDLER FOR OVERLAY
+
+    var showOverlay = function(link) {
+        $(".overlay").fadeIn();
+    };
+
+
+
+
     // EVENT HANDLER FOR OVERLAY (RETURN TO PORTFOLIO)
     $(".overlay").on("click", function() {
-
+        $(".overlay").fadeOut();
     });
 
     // EVENT HANDLER FOR BODY (RETURN TO HOME)
