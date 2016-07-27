1926a2d7167a2bbd37a6e7255fcc6f37e31cf8ea
Christopher Barnaby <cjbarnaby@gmail.com>
2016-07-28 08:23:33 +1000

    slideshow working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// GLOBALS
+////////////////////////////////
+//////////  GLOBALS ////////////
+////////////////////////////////
 
 var externalLinks = {
     linkedin: "https://www.linkedin.com/in/guy-harris",
@@ -19,10 +21,18 @@ var multi_image = {
     ]
 };
 
+////////////////////////////////
+//////////  UI //// ////////////
+////////////////////////////////
+
 $(document).ready(function() {
     $(".overlay").hide();
 
-    // EVENT HANDLERS FOR INTERNAL NAV LINKS
+////////////////////////////////
+//////////  NAVIGATION /////////
+////////////////////////////////
+
+    // NAVIGATION - EVENT HANDLERS FOR INTERNAL NAV LINKS
     $(".internal").on("click", function() {
         var link = $(this).attr("id");
         $(".overlay, .overlay-box").hide();
@@ -34,13 +44,26 @@ $(document).ready(function() {
         showOverlay(link);
     });
 
-    // EVENT HANDLERS FOR EXTERNAL NAV LINKS
+    // NAVIGATION - EVENT HANDLERS FOR EXTERNAL NAV LINKS
     $(".external").on("click", function() {
         var id = $(this).attr("id");
         window.open(externalLinks[id], '_blank');
     });
 
-    // EVENT HANDLER FOR OVERLAY
+////////////////////////////////
+//////////  ABOUT  /////////////
+////////////////////////////////
+
+    // ABOUT - FUNCTION TO DISPLAY ABOUT PAGE
+    var showAbout = function() {
+        $(".about-box").show();
+    };
+
+////////////////////////////////
+//////////  GENERAL ////////////
+////////////////////////////////
+
+    // GENERAL -  - EVENT HANDLER FOR CALLING FUNCTIONS TO DISPLAY PORTFOLIO OR ABOUT OVERLAYS
     var showOverlay = function(link) {
         $(".overlay").fadeIn();
         if (link === "portfolio") {
@@ -50,26 +73,39 @@ $(document).ready(function() {
         }
     };
 
-    // EVENT HANDLER FOR CONTENT TILE hover
+    // GENERAL - EVENT HANDLER FOR UNDERLAY/POSTER (RETURN TO HOME)
+    $(".underlay, .poster").on("click", function() {
+        $(".overlay-image").remove();
+        $(".filter-box").removeClass("filter");
+        $(".filter-box").addClass("unfilter");
+        $(".internal").removeClass("active");
+    });
+
+    // GENERAL - EVENT HANDLER FOR OVERLAY (RETURN TO PORTFOLIO / ABOUT)
+    $(".overlay").on("click", function() {
+        $(".overlay-image").remove();
+        $(".overlay").fadeOut();
+        $(".portfolio-box, .about-box").hide();
+        console.log("overlay");
+    });
+
+////////////////////////////////
+//////////  PORTFOLIO //////////
+////////////////////////////////
+
+    // PORTFOLIO - CONTENT TILE HOVER EFFECTS
     $(".content-tile").on("mouseenter", function() {
         $(this).find(".content-tile-text").css("opacity", 1);
     }).on("mouseleave", function() {
         $(this).find(".content-tile-text").css("opacity", 0);
     });
 
-    // FUNCTION TO DISPLAY PORTFOLIO INDEX
-
+    // PORTFOLIO - FUNCTION TO DISPLAY PORTFOLIO INDEX
     var showPortfolio = function() {
         $(".portfolio-box").show();
     };
 
-
-    // FUNCTION TO DISPLAY ABOUT PAGE
-    var showAbout = function() {
-        $(".about-box").show();
-    };
-
-    // EVENT HANDLERS FOR PORTFOLIO LINKS
+    // PORTFOLIO - EVENT HANDLERS FOR PORTFOLIO LINKS TO DISPLAY IMAGE
     $(".content-tile").on("click", function(e) {
         $(".overlay-image").remove();
         e.stopPropagation();
@@ -78,16 +114,23 @@ $(document).ready(function() {
         showImage(id);
     });
 
+    // PORTFOLIO -  FUNCTION FOR ADDING IMAGES
     var showImage = function(id) {
         if ((id === "ask_izzy") || (id === "maccas")) {
+            var $slideshow = $("<div></div>");
+            $slideshow.addClass("slideshow");
+            $slideshow.append( $("<div></div>").addClass("carousel") );
+            $(".overlay").append($slideshow);
+            $(".slideshow").fadeIn();
             for (var i = 0; i < multi_image[id].length; i++) {
-                addImage(multi_image[id][i]);
+                addSlideshowImage(multi_image[id][i]);
             }
         } else {
             addImage(id);
         }
     };
 
+    // PORTFOLIO - ADD SINGLE IMAGE TO OVERLAY
     var addImage = function(image) {
         var path = image + ".png";
         var url = "assets/" + path;
@@ -98,27 +141,28 @@ $(document).ready(function() {
         $(".overlay-image").fadeIn();
     };
 
+    // PORTFOLIO - ADD MULTI-IMAGE SET TO PORTFOLIO
+    var addSlideshowImage = function(image) {
+        var path = image + ".png";
+        var url = "assets/" + path;
+        var $img = $("<img></img>");
+        $img.addClass("slideshow-image");
+        $img.attr("src", url);
+        $(".carousel").append($img.hide().fadeIn(500));
+    };
 
-    // EVENT HANDLER FOR OVERLAY (RETURN TO PORTFOLIO / ABOUT)
-    $(".overlay").on("click", function() {
-        $(".overlay-image").remove();
-        $(".overlay").fadeOut();
-        $(".portfolio-box, .about-box").hide();
-        console.log("overlay");
-    });
-
-
+    // PORTFOLIO - EVENT HANDLER FOR PORTFOLIO IMAGE OVERLAY (RETURN TO PORTFOLIO)
     $(".overlay").on("click", ".overlay-image", function(e) {
         e.stopPropagation();
         e.stopImmediatePropagation();
         $(".overlay-image").remove();
     });
 
-    // EVENT HANDLER FOR BODY (RETURN TO HOME)
-    $(".underlay, .poster").on("click", function() {
-        $(".overlay-image").remove();
-        $(".filter-box").removeClass("filter");
-        $(".filter-box").addClass("unfilter");
-        $(".internal").removeClass("active");
+    // PORTFOLIO - EVENT HANDLER FOR SLIDESHOW (RETURN TO PORTFOLIO)
+    $(".overlay").on("click", ".carousel, .slideshow-image, .slideshow", function(e) {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        $(".slideshow").remove();
     });
+
 });
